384c4c07345b562a42d33f960db95fb85d091c8d
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2023-12-11 19:09:58 +0000

    variables commit

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const account_Id=1234
+let account_Email="manoranjanheremano@gmail.com"
+var accountpass="12345"
+accountCity="Rourkela"
+//account_Id=23//not allowed as const can't be changed;
+account_Email="mano@gmail.com";
+accountpass="123"
+let man;
+console.log(account_Id);
+console.table([account_Email,accountpass,account_Id,man])
