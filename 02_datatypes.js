6650c124ef8b2b8e674b000df52447265b84f9c7
Manoranjan Sahoo <116833948+Manoranjanhere@users.noreply.github.com>
2023-12-13 07:12:08 +0000

    add

---
+++ 02_datatypes.js
@@ -0,0 +1,13 @@
+let name = "Manoranjan"
+let age =18
+let islogged=true
+//number => 2 to the power 53;
+//bigint;
+//string =>"";
+//boolean =>false,true;
+//null=>stand alone value;
+//undefined=>   
+//symbol=>uniqueness;
+console.log(typeof "mano")
+console.log(typeof null)
+console.log(typeof undefined)
